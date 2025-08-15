71982d1316a09be632fb01238dd72f079980f079
soloplay <somnium1108@gmail.com>
2025-08-15 20:46:11 +0900

    삽입 정렬 구현

---
+++ InsertionSort/Main.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+void InsertionSort(int* array, int length)
+{
+	int temp;
+	for (int i = 1; i < length; ++i)
+	{
+		temp = array[i];
+		int j;
+		for (j = i - 1; j >= 0; --j)
+		{
+			if (array[j] <= temp) {
+				break;
+			}
+			array[j + 1] = array[j];
+		}
+		array[j + 1] = temp;
+	}
+}
+
+void Print(int* array, int length)
+{
+	for (int i = 0; i < length; ++i)
+	{
+		std::cout << array[i] << ' ';
+	}
+	std::cout << '\n';
+}
+
+int main()
+{
+	int array[] = { 5,4,5,2,1 };
+	int length = sizeof(array) / sizeof(array[0]);
+	
+	Print(array, length);
+	InsertionSort(array, length);
+	Print(array, length);
+}
