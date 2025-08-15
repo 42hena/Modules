03a0559a9b8ac09886f1d1798cb7e03b390f6d3e
soloplay <somnium1108@gmail.com>
2025-08-15 19:42:14 +0900

    버블 정렬 구현
    - 오름차순 용도로 테스트

---
+++ BubbleSort/Main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <algorithm>
+
+void BubbleSort(int* array, int length)
+{
+	for (int i = 0; i < length - 1; ++i)
+	{
+		for (int j = 0; j < length - 1 - i; ++j)
+		{
+			if (array[j] > array[j + 1]) {
+				std::swap(array[j], array[j + 1]);
+			}
+		}
+	}
+}
+
+void Print(int* array, int length)
+{
+	for (int i = 0; i < length; ++i)
+	{
+		std::cout << array[i] << ' ';
+	}
+	std::cout << '\n';
+}
+
+int main()
+{
+	int array[] = { 5,4,3,2,1 };
+	int length = 5;
+	Print(array, length);
+	BubbleSort(array, length);
+	Print(array, length);
+}
